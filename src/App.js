324284a34907f96bd3d3ab0af92ef2e73869eea2
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-08-14 11:11:44 -0500

    download works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import SpeedTest from "./components/SpeedTest";
+import "./index.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Local Internet Speed</h1>
+      <h1 className="title">Local Internet Speed</h1>
       <SpeedTest />
     </div>
   );
